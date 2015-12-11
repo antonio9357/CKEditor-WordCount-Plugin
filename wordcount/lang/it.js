0ebefcd4de098bbc59fe9d3c3a822ee32a3f54a7
antonio9357 <office@abarts.at>
2015-12-12 00:24:08 +0100

    Update it.js
    Translation completed

--- wordcount/lang/it.js
+++ wordcount/lang/it.js
@@ -1,14 +1,14 @@
 /*
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
-@author translation: Davide Montorio
+@author translation: Antonio Bortolin, Davide Montorio
 */
 CKEDITOR.plugins.setLang('wordcount', 'it', {
     WordCount: 'Parole:',
     CharCount: 'Caratteri:',
     CharCountWithHTML: 'Caratteri (HTML incluso):',
-    Paragraphs: 'Paragraphs:',
-    pasteWarning: 'Content can not be pasted because it is above the allowed limit',
-    Selected: 'Selected: ',
+    Paragraphs: 'Paragrafi:',
+    pasteWarning: 'Il contenuto non può essere incollato perchè supera il limite consentito.',
+    Selected: 'Selezionato: ',
     title: 'Statistiche'
 });
